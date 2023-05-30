bf3b72f42128167edb0835267824a46970980aac
cisc06 <francisco.ortega@abr.com>
2023-05-30 03:22:41 -0400

    implement simple AI

---
+++ Assets/Code/Ships/AIInputAdapter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Ships
+{
+    class AIInputAdapter : Input
+    {
+        private readonly Ship _ship;
+        private int _currentDirectionX;
+
+        public AIInputAdapter(Ship ship)
+        {
+            _ship = ship;
+            _currentDirectionX = 1;
+        }
+
+        public Vector2 GetDirection()
+        {
+            var viewportPoint = Camera.main.WorldToViewportPoint(_ship.transform.position);
+
+            if (viewportPoint.x < 0.05f)
+            {
+                _currentDirectionX = 1;
+            }else if (viewportPoint.x > 0.95f)
+            {
+                _currentDirectionX = -1;
+            }
+
+            return new Vector2(_currentDirectionX, 0);
+        }
+    }
+}
--- Assets/Code/Ships/ShipInstaller.cs
+++ Assets/Code/Ships/ShipInstaller.cs
@@ -4,24 +4,29 @@ namespace Ships
 {
    public class ShipInstaller : MonoBehaviour
    {
+       [SerializeField] private bool _useAI;
        [SerializeField] private bool _useJoystick;
        [SerializeField] private Joystick _joystick;
        [SerializeField] private Ship _ship;
 
        private void Awake()
        {
-           _ship.Configure(GetInput());
+            _ship.Configure(GetInput());
        }
 
        private Input GetInput()
        {
-           if (_useJoystick)
-           {
-               return new JoystickInputAdapter(_joystick);
-           }
+            if (_useAI)
+            {
+                return new AIInputAdapter(_ship);
+            }
+            if (_useJoystick)
+            {
+                return new JoystickInputAdapter(_joystick);
+            }
 
-           Destroy(_joystick.gameObject);
-           return new UnityInputAdapter();
+            Destroy(_joystick.gameObject);
+            return new UnityInputAdapter();
        }
    }
 }
