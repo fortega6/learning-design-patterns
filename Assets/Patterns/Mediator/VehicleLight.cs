e80f1882269d470a30ddb85c529d6cfb5cefe1e0
cisc06 <francisco.ortega@abr.com>
2023-06-04 00:11:28 -0400

    implment mediator pattern example

---
+++ Assets/Patterns/Mediator/VehicleLight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Patterns.Mediator
+{
+    public class VehicleLight : MonoBehaviour
+    {
+        [SerializeField] private Light _light;
+        public void TurnOn()
+        {
+            _light.enabled = true;
+        }
+
+        public void TurnOff()
+        {
+            _light.enabled = false;
+        }
+    }
+}
---
+++ Assets/Patterns/Mediator/Wheel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Patterns.Mediator
+{
+    public class Wheel : MonoBehaviour
+    {
+        public void AddFriction()
+        {
+        }
+
+        public void RemoveFriction()
+        {
+        }
+
+        public void TurnLeft()
+        {
+        }
+
+        public void TurnRight()
+        {
+        }
+    }
+}
