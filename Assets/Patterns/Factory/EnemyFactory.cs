6d6b8808a5932a64c5182e93441c017d1ff881a1
cisc06 <francisco.ortega@abr.com>
2023-06-06 03:47:01 -0400

    solution factory method v2.1

--- Assets/Patterns/Factory/EnemyFactory.cs
+++ Assets/Patterns/Factory/EnemyFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Patterns.Factory
@@ -6,11 +7,22 @@ namespace Patterns.Factory
     public class EnemyFactory : MonoBehaviour
     {
         [SerializeField] private Enemy[] _enemyPrefabs;
+        private Dictionary<string, Enemy> _idToEnemyPrefab;
+
+        private void Awake()
+        {
+            _idToEnemyPrefab = new Dictionary<string, Enemy>();
+            foreach (var enemyPrefab in _enemyPrefabs)
+            {
+                _idToEnemyPrefab.Add(enemyPrefab.Id, enemyPrefab);
+            }
+        }
 
         public void Create(string enemyId)
         {
-            var enemyPrefab = _enemyPrefabs.First(enemy => enemy.Id.Equals(enemyId));
+            //var enemyPrefab = _enemyPrefabs.First(enemy => enemy.Id.Equals(enemyId));
 
+            var enemyPrefab = _idToEnemyPrefab[enemyId];
             Instantiate(enemyPrefab, Random.onUnitSphere * 3, Quaternion.identity);
         }
     }
