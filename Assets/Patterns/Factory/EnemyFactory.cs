9adaed3630dc97d20297b503fb1d21ec370fb571
cisc06 <francisco.ortega@abr.com>
2023-06-06 02:54:46 -0400

    solution factory method v1.0

---
+++ Assets/Patterns/Factory/EnemyFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Patterns.Factory
+{
+    public class EnemyFactory : MonoBehaviour
+    {
+        [SerializeField] private Enemy[] _enemyPrefabs;
+
+        public void Create(string enemyId)
+        {
+            var enemyPrefab = _enemyPrefabs.First(enemy => enemy.Id.Equals(enemyId));
+
+            Instantiate(enemyPrefab, Random.onUnitSphere * 3, Quaternion.identity);
+        }
+    }
+}
--- Assets/Patterns/Factory/EnemySpawner.cs
+++ Assets/Patterns/Factory/EnemySpawner.cs
@@ -9,26 +9,21 @@ namespace Patterns.Factory
         [SerializeField] private string _enemyId1;
         [SerializeField] private string _enemyId2;
 
-        [SerializeField] private Enemy[] _enemyPrefabs;
+        [SerializeField] private EnemyFactory _enemyFactory;
 
         private void Update()
         {
             if (UnityEngine.Input.GetKeyDown(KeyCode.Q))
             {
-                CreateEnemy(_enemyId1);
+                _enemyFactory.Create(_enemyId1);
             }
 
             if (UnityEngine.Input.GetKeyDown(KeyCode.W))
             {
-                CreateEnemy(_enemyId2);
+                _enemyFactory.Create(_enemyId2);
             }
         }
 
-        private void CreateEnemy(string enemyId)
-        {
-            var enemyPrefab = _enemyPrefabs.First(enemy => enemy.Id.Equals(enemyId));
-
-            Instantiate(enemyPrefab, Random.onUnitSphere * 3, Quaternion.identity);
-        }
+        
     }
 }
