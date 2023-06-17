be77f425c812fc84f29682ee61a93e6089d16a0f
cisc06 <francisco.ortega@abr.com>
2023-06-17 02:31:08 -0400

    add new projectiles

---
+++ Assets/Code/Ships/Weapons/Projectiles/Projectile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Ships.Weapons.Projectiles
+{
+    public abstract class Projectile : MonoBehaviour
+    {
+        [SerializeField] private ProjectileId _id;
+        public string Id => _id.Value;
+    }
+}
---
+++ Assets/Code/Ships/Weapons/Projectiles/SinusoidalProjectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Ships.Weapons.Projectiles
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class SinusoidalProjectile : Projectile
+    {
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+        [SerializeField] private float _speed;
+        [SerializeField] private float _amplitude;
+        [SerializeField] private int _frequency;
+        
+        private float _currentTime;
+        private Vector3 _currentPosition;
+        private Transform _myTransform;
+
+        private void Start()
+        {
+            _currentTime = 0;
+            _myTransform = transform;
+            _currentPosition = transform.position;
+            StartCoroutine(DestroyIn(4));
+        }
+
+        private void FixedUpdate()
+        {
+            _currentPosition += _myTransform.up * (_speed * Time.deltaTime);
+            var horizontalPosition = _myTransform.right * (_amplitude * Mathf.Sin(_currentTime * _frequency));
+            Debug.Log(_currentPosition);
+            _rigidbody2D.MovePosition(_currentPosition + horizontalPosition);
+            _currentTime += Time.deltaTime;
+        }
+
+        private IEnumerator DestroyIn(float seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+            Destroy(gameObject);
+        }
+    }
+}
