06fe96d37232ee3f9a7458973711d02c549630ff
cisc06 <francisco.ortega@abr.com>
2023-06-28 02:50:51 -0400

    solution facade pattern example

--- Assets/Patterns/Facade/Menu.cs
+++ Assets/Patterns/Facade/Menu.cs
@@ -9,29 +9,23 @@ namespace Patterns.Facade
     {
         [SerializeField] private Button _saveButton;
         [SerializeField] private Button _loadButton;
-        
-        [SerializeField] private EnemySpawner _enemySpawner;
-        [SerializeField] private PlayerSpawner _playerSpawner;
-        private DataStore _dataStore;
 
+        [SerializeField] private GameFacade _gameFacade;
+        
         private void Awake()
         {
             _saveButton.onClick.AddListener(SaveGame);
             _loadButton.onClick.AddListener(LoadGame);
-            _dataStore = new PlayerPrefsDataStoreAdapter();
         }
 
         private void SaveGame()
         {
-            var saveData = new SaveData(_enemySpawner.Enemies,_playerSpawner.Player);
-            _dataStore.SetData(saveData, "SaveData");
+            _gameFacade.SaveGame();
         }
         
         private void LoadGame()
         {
-            var saveData = _dataStore.GetData<SaveData>("SaveData");
-            _enemySpawner.Enemies = saveData.Enemies;
-            _playerSpawner.Player = saveData.Player;
+            _gameFacade.LoadGame();
         }
     }
 }
