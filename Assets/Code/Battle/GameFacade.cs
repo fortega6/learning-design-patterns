1e1412a72b03e01e9755a5d9969ff13fc0cea69c
cisc06 <francisco.ortega@abr.com>
2023-06-29 02:11:28 -0400

    apply facade pattern

---
+++ Assets/Code/Battle/GameFacade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Ships;
+using Ships.Enemies;
+using UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Battle
+{
+    public class GameFacade : MonoBehaviour
+    {
+        [SerializeField] private ScreenFade _screenFade;
+        [SerializeField] private ShipInstaller _shipInstaller;
+        [SerializeField] private EnemySpawner _enemySpawner;
+
+        public void StartBattle()
+        {
+            _enemySpawner.StartSpawn();
+            _shipInstaller.SpawnUserShip();
+            _screenFade.Hide();
+        }
+
+        public void StopBattle()
+        {
+            _screenFade.Show();
+            _enemySpawner.StopAndReset();
+            _shipInstaller.DestroyUserShip();
+        }
+    }
+}
--- Assets/Code/UI/Menu.cs
+++ Assets/Code/UI/Menu.cs
@@ -1,4 +1,5 @@
 using System;
+using Battle;
 using Ships;
 using Ships.Enemies;
 using UnityEngine;
@@ -11,10 +12,7 @@ namespace UI
         [SerializeField] private Button _startBattleButton;
         [SerializeField] private Button _stopBattleButton;
 
-        [SerializeField] private ScreenFade _screenFade;
-        [SerializeField] private ShipInstaller _shipInstaller;
-        [SerializeField] private EnemySpawner _enemySpawner;
-
+        [SerializeField] private GameFacade _gameFacade;
         private void Awake()
         {
             _startBattleButton.onClick.AddListener(StartBattle);
@@ -23,16 +21,12 @@ namespace UI
 
         private void StartBattle()
         {
-            _enemySpawner.StartSpawn();
-            _shipInstaller.SpawnUserShip();
-            _screenFade.Hide();
+            _gameFacade.StartBattle();
         }
 
         private void StopBattle()
         {
-            _screenFade.Show();
-            _enemySpawner.StopAndReset();
-            _shipInstaller.DestroyUserShip();
+            _gameFacade.StopBattle();
         }
     }
 }
